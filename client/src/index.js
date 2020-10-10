436f486a6ab0f4241e354c4f3ebbda5b59b48012
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2020-10-10 16:11:16 +0900

    client

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import client from './apollo'
+import {ApolloProvider} from "@apollo/client"
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
